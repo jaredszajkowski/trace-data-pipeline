f01094977d32b79318d61c736b9a626393341622
Jared Szajkowski <jared.szajkowski@gmail.com>
2026-02-15 20:27:27 -0600

    Adding logging for cleaning elapsed time

--- stage0/create_daily_enhanced_trace.py
+++ stage0/create_daily_enhanced_trace.py
@@ -487,6 +487,8 @@ def clean_trace_data(
 
     sort_cols = ["cusip_id","trd_exctn_dt","trd_exctn_tm",
                  "trd_rpt_dt","trd_rpt_tm","msg_seq_nb"]
+
+    clean_start_time = time.time()  # Start total timer for cleaning process
     
     for i in range(0, len(cusip_chunks)):  
         start_time = time.time()  # Start timer
@@ -813,7 +815,12 @@ def clean_trace_data(
         elapsed_time = round(time.time() - start_time, 2)
         # logging.info(f"Chunk {i+1}: took {elapsed_time} seconds")
         logging.info(f"Chunk {i}: took {elapsed_time} seconds")
-        logging.info("-" * 50)  
+        logging.info("-" * 50)
+
+        # Log elapsed time for the cleaning process
+        clean_elapsed_time = round((time.time() - clean_start_time) / 60, 2)
+        logging.info(f"Total elapsed time: {clean_elapsed_time} minutes")
+        logging.info("-" * 50)
             
     if all_super_list:
         final_df = pd.concat(all_super_list, ignore_index=True)
